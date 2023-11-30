8240f8bbb33ecd9cf90b3b0cda53c135ce36849b
toranomonn <yuya8naito17@gmail.com>
2023-11-30 16:24:29 +0900

    fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -2,7 +2,23 @@
 
 int main()
 {
-	printf("git\n");
+	int i = 0;
+	for(i=0;i<=100;i++)
+	{
+		if(i%3 == 0 && i%5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if(i%3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if(i%5 == 0)
+		{
+			printf("Buzz\n");
+		}
+	}
+	
 
 	return 0;
 }
